db2599dd4d4145714e1373138ade0a58757f8bc5
saschawillems <webmaster@saschawillems.de>
2016-03-01 19:00:31 +0100

    Corrected texture target image barriers (dst instead of src)

--- offscreen/offscreen.cpp
+++ offscreen/offscreen.cpp
@@ -461,7 +461,7 @@ public:
 			offScreenFrameBuf.textureTarget.image,
 			VK_IMAGE_ASPECT_COLOR_BIT,
 			VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-			VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL);
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
 
 		// Blit offscreen color buffer to our texture target
 		VkImageBlit imgBlit;
@@ -514,7 +514,7 @@ public:
 			offScreenCmdBuffer,
 			offScreenFrameBuf.textureTarget.image,
 			VK_IMAGE_ASPECT_COLOR_BIT,
-			VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
 			VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
 		err = vkEndCommandBuffer(offScreenCmdBuffer);
