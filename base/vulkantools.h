a6a897f244977211bb80e12ecc170980172a3bea
saschawillems <webmaster@saschawillems.de>
2016-03-17 22:07:42 +0100

    Added function for checking against VK_SUCCESS, updated error list to latest SDK

--- base/vulkantools.h
+++ base/vulkantools.h
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <vector>
 #include <iostream>
+#include <stdexcept>
 #ifdef _WIN32
 #include <windows.h>
 #include <fcntl.h>
@@ -37,7 +38,9 @@ namespace vkTools
 	VkBool32 checkDeviceExtensionPresent(VkPhysicalDevice physicalDevice, const char* extensionName);
 	// Return string representation of a vulkan error string
 	std::string errorString(VkResult errorCode);
-	
+	// Asserts and outputs the error message if the result is not VK_SUCCESS
+	VkResult checkResult(VkResult result);
+
 	// Selected a suitable supported depth format starting with 32 bit down to 16 bit
 	// Returns false if none of the depth formats in the list is supported by the device
 	VkBool32 getSupportedDepthFormat(VkPhysicalDevice physicalDevice, VkFormat *depthFormat);
