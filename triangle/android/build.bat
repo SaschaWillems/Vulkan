0ffc005c8f60ffbe2821d6d0d9e4f7835f5db08c
saschawillems <webmaster@saschawillems.de>
2016-03-20 16:27:32 +0100

    Android makefiles, manifest and icon (#97)

--- triangle/android/build.bat
+++ triangle/android/build.bat
@@ -1,18 +1,22 @@
 cd jni
 call ndk-build
-if %ERRORLEVEL% GEQ 1 (
+if %ERRORLEVEL% EQU 0 (
 	echo ndk-build has failed, build cancelled
 	cd..
-	exit \b %errorlevel%
-)
-cd..
 
-mkdir "assets\shaders"
-xcopy "..\..\data\shaders\triangle.vert.spv" "assets\shaders" /Y
-xcopy "..\..\data\shaders\triangle.frag.spv" "assets\shaders" /Y
+	mkdir "assets\shaders"
+	xcopy "..\..\data\shaders\triangle.vert.spv" "assets\shaders" /Y
+	xcopy "..\..\data\shaders\triangle.frag.spv" "assets\shaders" /Y
+	
+	mkdir "res\drawable"
+	xcopy "..\..\android\images\icon.png" "res\drawable" /Y
 
-call ant debug -Dout.final.file=vulkanTriangle.apk
+	call ant debug -Dout.final.file=vulkanTriangle.apk
 
-if "%1" == "-deploy" (
-	adb install -r vulkanTriangle.apk
+	if "%1" == "-deploy" (
+		adb install -r vulkanTriangle.apk
+	)
+) ELSE (
+	echo error : ndk-build failed with errors!
+	cd..
 )
