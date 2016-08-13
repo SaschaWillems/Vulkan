299f5b2bae88ad4cbd3f67fb543aa3be1dc67085
saschawillems <webmaster@saschawillems.de>
2016-08-13 22:10:57 +0200

    Comment

--- radialblur/radialblur.cpp
+++ radialblur/radialblur.cpp
@@ -740,7 +740,7 @@ public:
 			&uniformData.scene.memory,
 			&uniformData.scene.descriptor);
 
-		// Fullscreen quad fragment shader uniform buffer
+		// Fullscreen radialb blur parameters
 		createBuffer(
 			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
