20ed54bbb6a0ebfac922f8ac010ba914207a19b6
saschawillems <webmaster@saschawillems.de>
2017-02-09 20:28:38 +0100

    Removed unneeded includes

--- base/VulkanTexture.hpp
+++ base/VulkanTexture.hpp
@@ -12,18 +12,10 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include <map>
 
 #include "vulkan/vulkan.h"
 
-#include <assimp/Importer.hpp> 
-#include <assimp/scene.h>     
-#include <assimp/postprocess.h>
-#include <assimp/cimport.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <gli/gli.hpp>
 
 #include "vulkantools.h"
 #include "vulkandevice.hpp"
