6f9db79801075bba4dc917b7bb2e4f63ba9f52d1
saschawillems <webmaster@saschawillems.de>
2017-02-11 09:44:46 +0100

    Missing include

--- base/camera.hpp
+++ base/camera.hpp
@@ -10,7 +10,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 class Camera
 {
