ed6451a9569a4c4994ad35cc5e94926c563bd0aa
saschawillems <webmaster@saschawillems.de>
2017-02-26 18:57:41 +0100

    Continued work on basic PBR example

--- data/shaders/pbr/pbr.frag
+++ data/shaders/pbr/pbr.frag
@@ -1,9 +1,5 @@
 #version 450
 
-layout (binding = 1) uniform samplerCube envmap;
-layout (binding = 2) uniform samplerCube envmapibldiff;
-layout (binding = 3) uniform samplerCube envmapiblrefl;
-
 layout (location = 0) in vec3 inWorldPos;
 layout (location = 1) in vec3 inNormal;
 layout (location = 2) in vec2 inUV;
@@ -15,6 +11,10 @@ layout (binding = 0) uniform UBO
 	mat4 view;
 	vec3 camPos;
 } ubo;
+
+layout (binding = 1) uniform UBOShared {
+	vec4 lights[4];
+} uboParams;
 
 layout (location = 0) out vec4 outColor;
 
@@ -30,117 +30,98 @@ const float PI = 3.14159265359;
 
 //#define ROUGHNESS_PATTERN 1
 
-// Fresnel ------------------------------------------------------------------------
-
-float fresnelSchlick(float ct, float F0)
+vec3 materialcolor()
 {
-	return F0 + (1.0 - F0) * pow(1.0 - ct, 5.0);
+	return vec3(material.r, material.g, material.b);
 }
 
-// Normal distribution functions ---------------------------------------------------
-
-float NDF_blinnPhong(float dotNH, float alphaSqr)
-{
-	return 1.0 / (PI * alphaSqr) * pow(dotNH, 2.0 / alphaSqr - 2.0);
-}
-
-float NDF_beckmann(float dotNH, float alphaSqr)
-{
-	float dotNH2 = dotNH * dotNH;
-	return 1.0 / (PI * alphaSqr * dotNH2 * dotNH2) * exp((dotNH2 - 1.0) / (alphaSqr * dotNH2));
-}
-
-float NDF_GGX(float dotNH, float alphaSqr)
-{
-	return alphaSqr / (PI * pow(dotNH * dotNH * (alphaSqr - 1.0) + 1.0, 2.0));
-}
-
-// Geometry visibility functions ---------------------------------------------------
-
-float GEOM_SchlickSmith(float dotNL, float dotNV, float alpha)
-{
-	//float k = alpha * sqrt(2.0 / PI);
-	float k = pow(0.8 + 0.5 * alpha, 2.0) / 2.0;
-	float GL = 1.0 / (dotNL * (1.0 - k) + k);
-	float GV = 1.0 / (dotNV * (1.0 - k) + k);
-	return GL * GV;
-
-}
-
-float PBR_Shade(vec3 N, vec3 V, vec3 L, float roughness, float F0) 
+// Normal Distribution function --------------------------------------
+float D_GGX(float dotNH, float roughness)
 {
 	float alpha = roughness * roughness;
-	float alphaSqr = alpha * alpha;
+	float alpha2 = alpha * alpha;
+	float denom = dotNH * dotNH * (alpha2 - 1.0) + 1.0;
+	return (alpha2)/(PI * denom*denom); 
+}
 
+// Geometric Shadowing function --------------------------------------
+float G_SchlickmithGGX(float dotNL, float dotNV, float roughness)
+{
+	float r = (roughness + 1.0);
+	float k = (r*r) / 8.0;
+	float GL = dotNL / (dotNL * (1.0 - k) + k);
+	float GV = dotNV / (dotNV * (1.0 - k) + k);
+	return GL * GV;
+}
+
+// Fresnel function ----------------------------------------------------
+vec3 F_Schlick(float cosTheta, float metallic)
+{
+	vec3 F0 = mix(vec3(0.04), materialcolor(), metallic); // * material.specular
+	vec3 F = F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0); 
+	return F;    
+}
+
+// Specular BRDF composition --------------------------------------------
+
+vec3 BRDF(vec3 L, vec3 V, vec3 N, float metallic, float roughness)
+{
+	// Precalculate vectors and dot products	
 	vec3 H = normalize (V + L);
-
-	float dotNL = clamp(dot(N, L), 0.0, 1.0);
 	float dotNV = clamp(dot(N, V), 0.0, 1.0);
-	float dotNH = clamp(dot(N, H), 0.0, 1.0);
+	float dotNL = clamp(dot(N, L), 0.0, 1.0);
 	float dotLH = clamp(dot(L, H), 0.0, 1.0);
+	float dotNH = clamp(dot(N, H), 0.0, 1.0);
 
-	// Normal distribution
-	float Di = NDF_GGX(dotNH, alphaSqr);
-	// Fresnel
-	float Fs = fresnelSchlick(dotNV, F0);
-	// Visibility term 
-	float Vs = GEOM_SchlickSmith(dotNL, dotNV, alpha);
+	// Light color fixed
+	vec3 lightColor = vec3(1.0);
 
-	return Di * Fs * Vs;
+	vec3 color = vec3(0.0);
+
+	if (dotNL > 0.0)
+	{
+		float rroughness = max(0.05, roughness);
+		// D = Normal distribution (Distribution of the microfacets)
+		float D = D_GGX(dotNH, roughness); 
+		// G = Geometric shadowing term (Microfacets shadowing)
+		float G = G_SchlickmithGGX(dotNL, dotNV, roughness);
+		// F = Fresnel factor (Reflectance depending on angle of incidence)
+		vec3 F = F_Schlick(dotNV, metallic);
+
+		vec3 spec = D * F * G / (4.0 * dotNL * dotNV);
+
+		color += spec * dotNL * lightColor;
+	}
+
+	return color;
 }
 
 // ----------------------------------------------------------------------------
 void main()
-{		
-	// Partially based on https://www.shadertoy.com/view/XsfXWX by Alexander Alekseev (https://github.com/tdmaav)
-
-	// One fixed light source
-	vec3 lightPos = vec3(-10.0f, -10.0f, 10.0f);
-	// Take light color from environment map
-	vec3 lightColor = texture(envmapiblrefl, vec3(0.5)).xyz;
-
+{		  
 	vec3 N = normalize(inNormal);
 	vec3 V = normalize(ubo.camPos - inWorldPos);
-	vec3 L = normalize(lightPos - inWorldPos);
-	vec3 R = reflect(-V, N); 
 
-	// Store material values for quick testing/changing inside the shader
 	float roughness = material.roughness;
-	float metallic = material.metallic;
 
 	// Add striped pattern to roughness based on vertex position
 #ifdef ROUGHNESS_PATTERN
 	roughness = max(roughness, step(fract(inWorldPos.y * 2.02), 0.5));
 #endif
 
-	// Get IBL components from cube maps
-	vec3 IBLdiffuse = texture(envmapibldiff, inNormal).rgb;
-	vec3 IBLreflection = texture(envmapiblrefl, inNormal).rgb;
+	// Specular contribution
+	vec3 Lo = vec3(0.0);
+	for (int i = 0; i < uboParams.lights.length(); i++) {
+		vec3 L = normalize(uboParams.lights[i].xyz - inWorldPos);
+		Lo += BRDF(L, V, N, material.metallic, roughness);
+	};
 
-	// Fresnel part
-	float fresnel = pow(max(1.0 - abs(dot(N, V)), 0.0), 1.5f + roughness);    
-		
-	// Reflection part        
+	// Combine with ambient
+	vec3 color = materialcolor() * 0.02;
+	color += Lo;
 
-	// Select mip level based on roughness
-	ivec2 dim = textureSize(envmap, 0);
-	float nummips = log2(max(dim.s, dim.y));
-	vec3 reflection = texture(envmap, R).xyz;
-	reflection = textureLod(envmap, R, max(roughness * nummips, textureQueryLod(envmap, R).y)).rgb;
-	reflection = mix(reflection, IBLreflection, (1.0-fresnel) * roughness);
-	reflection = mix(reflection, IBLreflection, roughness);
-		
-	// Specular part
-	// F0 based on metallic factor of material
-	vec3 F0 = vec3(0.04); 
-	F0 = mix(F0, lightColor, material.metallic);
-	vec3 spec = lightColor * PBR_Shade(N, V, L, roughness, F0.r);
-	reflection -= spec;
-		
-	// Diffuse part
-	vec3 matColor = vec3(material.r, material.g, material.b);
-	vec3 diffuse = mix(IBLdiffuse * matColor, reflection, fresnel);
+	// Gamma correct
+	color = pow(color, vec3(0.4545));
 
-	// Final output mixes based on material metalness
-	outColor.rgb = mix(diffuse, reflection, metallic) + spec;
+	outColor = vec4(color, 1.0);
 }
--- pbr/pbr.cpp
+++ pbr/pbr.cpp
@@ -1,7 +1,9 @@
 /*
-* Vulkan Example - Physical based rendering (incl. IBL)
+* Vulkan Example - Physical based shading basics
 *
-* Copyright (C) 2016 by Sascha Willems - www.saschawillems.de
+* See http://graphicrants.blogspot.de/2013/08/specular-brdf-reference.html for a good reference to the different functions that make up a specular BRDF
+*
+* Copyright (C) 2017 by Sascha Willems - www.saschawillems.de
 *
 * This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
 */
@@ -22,7 +24,6 @@
 #include <vulkan/vulkan.h>
 #include "vulkanexamplebase.h"
 #include "VulkanBuffer.hpp"
-#include "VulkanTexture.hpp"
 #include "VulkanModel.hpp"
 
 #define VERTEX_BUFFER_BIND_ID 0
@@ -33,18 +34,15 @@
 struct Material {
 	float roughness;
 	float metallic;
-	float r,g,b;	// Color components as single floats because we use push constants
+	float r, g, b;	// Color components as single floats because we use push constants
+	std::string name;
+	Material() {};
+	Material(std::string n, glm::vec3 c, float r, float m) : name(n), roughness(r), metallic(m), r(c.r), g(c.g), b(c.b) { };
 };
 
 class VulkanExample : public VulkanExampleBase
 {
 public:
-	bool displaySkybox = true;
-
-	vks::TextureCubeMap envmap;
-	vks::TextureCubeMap envmapiblDiff;
-	vks::TextureCubeMap envmapiblRefl;
-
 	// Vertex layout for the models
 	vks::VertexLayout vertexLayout = vks::VertexLayout({
 		vks::VERTEX_COMPONENT_POSITION,
@@ -61,46 +59,62 @@ public:
 	struct {
 		vks::Buffer object;
 		vks::Buffer skybox;
+		vks::Buffer params;
 	} uniformBuffers;
 
-	struct UBOVS {
+	struct UBOMatrices {
 		glm::mat4 projection;
 		glm::mat4 model;
 		glm::mat4 view;
 		glm::vec3 camPos;
-	} uboVS;
+	} uboMatrices;
 
-	struct {
-		VkPipeline skybox;
-		VkPipeline pbr;
-	} pipelines;
-
-	struct {
-		VkDescriptorSet object;
-		VkDescriptorSet skybox;
-	} descriptorSets;
+	struct UBOParams {
+		glm::vec4 lights[4];
+	} uboParams;
 
 	VkPipelineLayout pipelineLayout;
+	VkPipeline pipeline;
 	VkDescriptorSetLayout descriptorSetLayout;
+	VkDescriptorSet descriptorSet;
+
+	// Default materials to select from
+	std::vector<Material> materials;
+	int32_t materialIndex = 0;
 
 	VulkanExample() : VulkanExampleBase(ENABLE_VALIDATION)
 	{
-		title = "Vulkan Example - Physical based rendering";
+		title = "Vulkan Example - Physical based shading basics";
 		enableTextOverlay = true;
 		camera.type = Camera::CameraType::firstperson;
-		camera.setPosition(glm::vec3(8.0f, 7.25f, -13.0f));
-		camera.setRotation(glm::vec3(-31.0f, 24.0f, 0.0f));
+		camera.setPosition(glm::vec3(13.0f, 8.0f, -10.0f));
+		camera.setRotation(glm::vec3(-31.75f, 45.0f, 0.0f));
 		camera.movementSpeed = 4.0f;
 		camera.setPerspective(60.0f, (float)width / (float)height, 0.1f, 256.0f);
 		camera.rotationSpeed = 0.25f;
-		width = 1920;
-		height = 1080;
+		paused = true;
+		timerSpeed *= 0.25f;
+
+		// Setup some default materials (source: https://seblagarde.wordpress.com/2011/08/17/feeding-a-physical-based-lighting-mode/)
+		materials.push_back(Material("Gold", glm::vec3(1.0f, 0.765557f, 0.336057f), 0.1f, 1.0f));
+		materials.push_back(Material("Copper", glm::vec3(0.955008f, 0.637427f, 0.538163f), 0.1f, 1.0f));
+		materials.push_back(Material("Chromium", glm::vec3(0.549585f, 0.556114f, 0.554256f), 0.1f, 1.0f));
+		materials.push_back(Material("Nickel", glm::vec3(0.659777f, 0.608679f, 0.525649f), 0.1f, 1.0f));
+		materials.push_back(Material("Titanium", glm::vec3(0.541931f, 0.496791f, 0.449419f), 0.1f, 1.0f));
+		materials.push_back(Material("Cobalt", glm::vec3(0.662124f, 0.654864f, 0.633732f), 0.1f, 1.0f));
+		materials.push_back(Material("Platinum", glm::vec3(0.672411f, 0.637331f, 0.585456f), 0.1f, 1.0f));
+		// Testing materials
+		materials.push_back(Material("White", glm::vec3(1.0f), 0.1f, 1.0f));
+		materials.push_back(Material("Red", glm::vec3(1.0f, 0.0f, 0.0f), 0.1f, 1.0f));
+		materials.push_back(Material("Blue", glm::vec3(0.0f, 0.0f, 1.0f), 0.1f, 1.0f));
+		materials.push_back(Material("Black", glm::vec3(0.0f), 0.1f, 1.0f));
+
+		materialIndex = 8;
 	}
 
 	~VulkanExample()
-	{
-		vkDestroyPipeline(device, pipelines.skybox, nullptr);
-		vkDestroyPipeline(device, pipelines.pbr, nullptr);
+	{		
+		vkDestroyPipeline(device, pipeline, nullptr);
 
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
@@ -112,9 +126,7 @@ public:
 
 		uniformBuffers.object.destroy();
 		uniformBuffers.skybox.destroy();
-		envmap.destroy();
-		envmapiblDiff.destroy();
-		envmapiblRefl.destroy();
+		uniformBuffers.params.destroy();
 	}
 
 	void reBuildCommandBuffers()
@@ -153,50 +165,42 @@ public:
 
 			vkCmdBeginRenderPass(drawCmdBuffers[i], &renderPassBeginInfo, VK_SUBPASS_CONTENTS_INLINE);
 
-			VkViewport viewport = vks::initializers::viewport((float)width,	(float)height, 0.0f, 1.0f);
+			VkViewport viewport = vks::initializers::viewport((float)width, (float)height, 0.0f, 1.0f);
 			vkCmdSetViewport(drawCmdBuffers[i], 0, 1, &viewport);
 
-			VkRect2D scissor = vks::initializers::rect2D(width,	height,	0, 0);
+			VkRect2D scissor = vks::initializers::rect2D(width, height, 0, 0);
 			vkCmdSetScissor(drawCmdBuffers[i], 0, 1, &scissor);
 
 			VkDeviceSize offsets[1] = { 0 };
 
-			// Skybox
-			if (displaySkybox)
-			{
-				vkCmdBindDescriptorSets(drawCmdBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &descriptorSets.skybox, 0, NULL);
-				vkCmdBindVertexBuffers(drawCmdBuffers[i], VERTEX_BUFFER_BIND_ID, 1, &models.skybox.vertices.buffer, offsets);
-				vkCmdBindIndexBuffer(drawCmdBuffers[i], models.skybox.indices.buffer, 0, VK_INDEX_TYPE_UINT32);
-				vkCmdBindPipeline(drawCmdBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipelines.skybox);
-				vkCmdDrawIndexed(drawCmdBuffers[i], models.skybox.indexCount, 1, 0, 0, 0);
-			}
-
 			// Objects
-			vkCmdBindDescriptorSets(drawCmdBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &descriptorSets.object, 0, NULL);
+			vkCmdBindPipeline(drawCmdBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+			vkCmdBindDescriptorSets(drawCmdBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &descriptorSet, 0, NULL);
 			vkCmdBindVertexBuffers(drawCmdBuffers[i], VERTEX_BUFFER_BIND_ID, 1, &models.objects[models.objectIndex].vertices.buffer, offsets);
 			vkCmdBindIndexBuffer(drawCmdBuffers[i], models.objects[models.objectIndex].indices.buffer, 0, VK_INDEX_TYPE_UINT32);
-			vkCmdBindPipeline(drawCmdBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipelines.pbr);
 
-			Material mat;
-			mat.r = 1.0f;
-			mat.g = 0.0f;
-			mat.b = 0.0f;
+			Material mat = materials[materialIndex];
 
-//#define SINGLE_MESH 1	
+			//#define SINGLE_MESH 1	
 #ifdef SINGLE_MESH
-			mat.metallic = 0.1;
-			mat.roughness = 1.0;
-			glm::vec3 pos = glm::vec3(0.0f);
-			vkCmdPushConstants(drawCmdBuffers[i], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(glm::vec3), &pos);
-			vkCmdPushConstants(drawCmdBuffers[i], pipelineLayout, VK_SHADER_STAGE_FRAGMENT_BIT, sizeof(glm::vec3), sizeof(Material), &mat);
-			vkCmdDrawIndexed(drawCmdBuffers[i], models.objects[models.objectIndex].indexCount, 1, 0, 0, 0);
+			mat.metallic = 1.0;
+			mat.roughness = 0.1;
+
+			uint32_t objcount = 10;
+			for (uint32_t x = 0; x < objcount; x++) {
+				glm::vec3 pos = glm::vec3(float(x - (objcount / 2.0f)) * 2.5f, 0.0f, 0.0f);
+				mat.roughness = glm::clamp((float)x / (float)objcount, 0.005f, 1.0f);
+				vkCmdPushConstants(drawCmdBuffers[i], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(glm::vec3), &pos);
+				vkCmdPushConstants(drawCmdBuffers[i], pipelineLayout, VK_SHADER_STAGE_FRAGMENT_BIT, sizeof(glm::vec3), sizeof(Material), &mat);
+				vkCmdDrawIndexed(drawCmdBuffers[i], models.objects[models.objectIndex].indexCount, 1, 0, 0, 0);
+			}
 #else
 			for (uint32_t y = 0; y < GRID_DIM; y++) {
 				for (uint32_t x = 0; x < GRID_DIM; x++) {
 					glm::vec3 pos = glm::vec3(float(x - (GRID_DIM / 2.0f)) * 2.5f, 0.0f, float(y - (GRID_DIM / 2.0f)) * 2.5f);
 					vkCmdPushConstants(drawCmdBuffers[i], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(glm::vec3), &pos);
-					mat.metallic = (float)x / (float)GRID_DIM;
-					mat.roughness = (float)y / (float)GRID_DIM;
+					mat.metallic = (float)x / (float)(GRID_DIM - 1);
+					mat.roughness = glm::clamp((float)y / (float)(GRID_DIM - 1), 0.05f, 1.0f);
 					vkCmdPushConstants(drawCmdBuffers[i], pipelineLayout, VK_SHADER_STAGE_FRAGMENT_BIT, sizeof(glm::vec3), sizeof(Material), &mat);
 					vkCmdDrawIndexed(drawCmdBuffers[i], models.objects[models.objectIndex].indexCount, 1, 0, 0, 0);
 				}
@@ -213,28 +217,22 @@ public:
 		// Skybox
 		models.skybox.loadFromFile(getAssetPath() + "models/cube.obj", vertexLayout, 1.0f, vulkanDevice, queue);
 		// Objects
-		std::vector<std::string> filenames = { "geosphere.obj", "teapot.dae", "torusknot.obj", "suzanne.obj" };
+		std::vector<std::string> filenames = { "geosphere.obj", "teapot.dae", "torusknot.obj", "venus.fbx" };
 		for (auto file : filenames) {
 			vks::Model model;
-			model.loadFromFile(getAssetPath() + "models/" + file, vertexLayout, OBJ_DIM * (file == "suzanne.obj" ? 2.0f : 1.0f), vulkanDevice, queue);
+			model.loadFromFile(getAssetPath() + "models/" + file, vertexLayout, OBJ_DIM * (file == "venus.fbx" ? 3.0f : 1.0f), vulkanDevice, queue);
 			models.objects.push_back(model);
 		}
-		// Example uses three different cubemaps (environment, diffuse for IBL (irradiance) and reflective for IBL)
-		envmap.loadFromFile(getAssetPath() + "textures/cubemap_uffizi_env.dds", VK_FORMAT_BC3_UNORM_BLOCK, vulkanDevice, queue);
-		envmapiblDiff.loadFromFile(getAssetPath() + "textures/cubemap_uffizi_ibl_diff.dds", VK_FORMAT_R8G8B8A8_UNORM, vulkanDevice, queue);
-		envmapiblRefl.loadFromFile(getAssetPath() + "textures/cubemap_uffizi_ibl_refl.dds", VK_FORMAT_R8G8B8A8_UNORM, vulkanDevice, queue);
 	}
 
 	void setupDescriptorSetLayout()
 	{
 		std::vector<VkDescriptorSetLayoutBinding> setLayoutBindings = {
 			vks::initializers::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0),
-			vks::initializers::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT, 1),
-			vks::initializers::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT, 2),
-			vks::initializers::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT, 3),
+			vks::initializers::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_FRAGMENT_BIT, 1),
 		};
 
-		VkDescriptorSetLayoutCreateInfo descriptorLayout = 
+		VkDescriptorSetLayoutCreateInfo descriptorLayout =
 			vks::initializers::descriptorSetLayoutCreateInfo(setLayoutBindings);
 
 		VK_CHECK_RESULT(vkCreateDescriptorSetLayout(device, &descriptorLayout, nullptr, &descriptorSetLayout));
@@ -257,8 +255,7 @@ public:
 	{
 		// Descriptor Pool
 		std::vector<VkDescriptorPoolSize> poolSizes = {
-			vks::initializers::descriptorPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 2),
-			vks::initializers::descriptorPoolSize(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 6)
+			vks::initializers::descriptorPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 4),
 		};
 
 		VkDescriptorPoolCreateInfo descriptorPoolInfo =
@@ -267,27 +264,16 @@ public:
 		VK_CHECK_RESULT(vkCreateDescriptorPool(device, &descriptorPoolInfo, nullptr, &descriptorPool));
 
 		// Descriptor sets
-		
+
 		VkDescriptorSetAllocateInfo allocInfo =
 			vks::initializers::descriptorSetAllocateInfo(descriptorPool, &descriptorSetLayout, 1);
 
 		// 3D object descriptor set
-		VK_CHECK_RESULT(vkAllocateDescriptorSets(device, &allocInfo, &descriptorSets.object));
+		VK_CHECK_RESULT(vkAllocateDescriptorSets(device, &allocInfo, &descriptorSet));
 
 		std::vector<VkWriteDescriptorSet> writeDescriptorSets = {
-			vks::initializers::writeDescriptorSet(descriptorSets.object, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 0, &uniformBuffers.object.descriptor),
-			vks::initializers::writeDescriptorSet(descriptorSets.object, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, &envmap.descriptor),
-			vks::initializers::writeDescriptorSet(descriptorSets.object, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 2, &envmapiblDiff.descriptor),
-			vks::initializers::writeDescriptorSet(descriptorSets.object, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 3, &envmapiblRefl.descriptor),
-		};
-		vkUpdateDescriptorSets(device, static_cast<uint32_t>(writeDescriptorSets.size()), writeDescriptorSets.data(), 0, NULL);
-
-		// Sky box descriptor set
-		VK_CHECK_RESULT(vkAllocateDescriptorSets(device, &allocInfo, &descriptorSets.skybox));
-
-		writeDescriptorSets = {
-			vks::initializers::writeDescriptorSet(descriptorSets.skybox, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 0, &uniformBuffers.skybox.descriptor),
-			vks::initializers::writeDescriptorSet(descriptorSets.skybox, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, &envmapiblRefl.descriptor),
+			vks::initializers::writeDescriptorSet(descriptorSet, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 0, &uniformBuffers.object.descriptor),
+			vks::initializers::writeDescriptorSet(descriptorSet, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, &uniformBuffers.params.descriptor),
 		};
 		vkUpdateDescriptorSets(device, static_cast<uint32_t>(writeDescriptorSets.size()), writeDescriptorSets.data(), 0, NULL);
 	}
@@ -358,11 +344,6 @@ public:
 
 		pipelineCreateInfo.pVertexInputState = &vertexInputState;
 
-		// Skybox pipeline (background cube)
-		shaderStages[0] = loadShader(getAssetPath() + "shaders/pbr/skybox.vert.spv", VK_SHADER_STAGE_VERTEX_BIT);
-		shaderStages[1] = loadShader(getAssetPath() + "shaders/pbr/skybox.frag.spv", VK_SHADER_STAGE_FRAGMENT_BIT);
-		VK_CHECK_RESULT(vkCreateGraphicsPipelines(device, pipelineCache, 1, &pipelineCreateInfo, nullptr, &pipelines.skybox));
-
 		// PBR pipeline
 		shaderStages[0] = loadShader(getAssetPath() + "shaders/pbr/pbr.vert.spv", VK_SHADER_STAGE_VERTEX_BIT);
 		shaderStages[1] = loadShader(getAssetPath() + "shaders/pbr/pbr.frag.spv", VK_SHADER_STAGE_FRAGMENT_BIT);
@@ -370,8 +351,8 @@ public:
 		depthStencilState.depthWriteEnable = VK_TRUE;
 		depthStencilState.depthTestEnable = VK_TRUE;
 		// Flip cull mode
-		rasterizationState.cullMode = VK_CULL_MODE_NONE;
-		VK_CHECK_RESULT(vkCreateGraphicsPipelines(device, pipelineCache, 1, &pipelineCreateInfo, nullptr, &pipelines.pbr));
+		rasterizationState.cullMode = VK_CULL_MODE_FRONT_BIT;
+		VK_CHECK_RESULT(vkCreateGraphicsPipelines(device, pipelineCache, 1, &pipelineCreateInfo, nullptr, &pipeline));
 	}
 
 	// Prepare and initialize uniform buffer containing shader uniforms
@@ -382,34 +363,62 @@ public:
 			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
 			&uniformBuffers.object,
-			sizeof(uboVS)));
+			sizeof(uboMatrices)));
 
 		// Skybox vertex shader uniform buffer
 		VK_CHECK_RESULT(vulkanDevice->createBuffer(
 			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
 			&uniformBuffers.skybox,
-			sizeof(uboVS)));
+			sizeof(uboMatrices)));
+
+		// Shared parameter uniform buffer
+		VK_CHECK_RESULT(vulkanDevice->createBuffer(
+			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			&uniformBuffers.params,
+			sizeof(uboParams)));
 
 		// Map persistent
 		VK_CHECK_RESULT(uniformBuffers.object.map());
 		VK_CHECK_RESULT(uniformBuffers.skybox.map());
+		VK_CHECK_RESULT(uniformBuffers.params.map());
 
 		updateUniformBuffers();
+		updateLights();
 	}
 
 	void updateUniformBuffers()
 	{
 		// 3D object
-		uboVS.projection = camera.matrices.perspective;
-		uboVS.view = camera.matrices.view;
-		uboVS.model = glm::rotate(glm::mat4(), glm::radians(-45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		uboVS.camPos = camera.position * -1.0f;
-		memcpy(uniformBuffers.object.mapped, &uboVS, sizeof(uboVS));
+		uboMatrices.projection = camera.matrices.perspective;
+		uboMatrices.view = camera.matrices.view;
+		uboMatrices.model = glm::rotate(glm::mat4(), glm::radians(-90.0f + (models.objectIndex == 1 ? 45.0f : 0.0f)), glm::vec3(0.0f, 1.0f, 0.0f));
+		uboMatrices.camPos = camera.position * -1.0f;
+		memcpy(uniformBuffers.object.mapped, &uboMatrices, sizeof(uboMatrices));
 
 		// Skybox
-		uboVS.model = glm::mat4(glm::mat3(camera.matrices.view));
-		memcpy(uniformBuffers.skybox.mapped, &uboVS, sizeof(uboVS));
+		uboMatrices.model = glm::mat4(glm::mat3(camera.matrices.view));
+		memcpy(uniformBuffers.skybox.mapped, &uboMatrices, sizeof(uboMatrices));
+	}
+
+	void updateLights()
+	{
+		const float p = 15.0f;
+		uboParams.lights[0] = glm::vec4(-p, -p*0.5f, -p, 1.0f);
+		uboParams.lights[1] = glm::vec4(-p, -p*0.5f,  p, 1.0f);
+		uboParams.lights[2] = glm::vec4( p, -p*0.5f,  p, 1.0f);
+		uboParams.lights[3] = glm::vec4( p, -p*0.5f, -p, 1.0f);
+
+		if (!paused)
+		{
+			uboParams.lights[0].x = sin(glm::radians(timer * 360.0f)) * 20.0f;
+			uboParams.lights[0].z = cos(glm::radians(timer * 360.0f)) * 20.0f;
+			uboParams.lights[1].x = cos(glm::radians(timer * 360.0f)) * 20.0f;
+			uboParams.lights[1].y = sin(glm::radians(timer * 360.0f)) * 20.0f;
+		}
+
+		memcpy(uniformBuffers.params.mapped, &uboParams, sizeof(uboParams));
 	}
 
 	void draw()
@@ -440,17 +449,14 @@ public:
 		if (!prepared)
 			return;
 		draw();
+		if (!paused)
+			updateLights();
 	}
 
 	virtual void viewChanged()
 	{
 		updateUniformBuffers();
-	}
-
-	void toggleSkyBox()
-	{
-		displaySkybox = !displaySkybox;
-		reBuildCommandBuffers();
+		updateTextOverlay();
 	}
 
 	void toggleObject()
@@ -460,20 +466,38 @@ public:
 		{
 			models.objectIndex = 0;
 		}
+		updateUniformBuffers();
 		reBuildCommandBuffers();
+	}
+
+	void toggleMaterial(int32_t dir)
+	{
+		materialIndex += dir;
+		if (materialIndex < 0) {
+			materialIndex = static_cast<int32_t>(materials.size()) - 1;
+		}
+		if (materialIndex > static_cast<int32_t>(materials.size()) - 1) {
+			materialIndex = 0;
+		}
+		reBuildCommandBuffers();
+		updateTextOverlay();
 	}
 
 	virtual void keyPressed(uint32_t keyCode)
 	{
 		switch (keyCode)
 		{
-		case KEY_F2:
-		case GAMEPAD_BUTTON_A:
-			toggleSkyBox();
-			break;
 		case KEY_SPACE:
 		case GAMEPAD_BUTTON_X:
 			toggleObject();
+			break;
+		case KEY_KPADD:
+		case GAMEPAD_BUTTON_R1:
+			toggleMaterial(1);
+			break;
+		case KEY_KPSUB:
+		case GAMEPAD_BUTTON_L1:
+			toggleMaterial(-1);
 			break;
 		}
 	}
@@ -481,10 +505,10 @@ public:
 	virtual void getOverlayText(VulkanTextOverlay *textOverlay)
 	{
 #if defined(__ANDROID__)
-		textOverlay->addText("\"Button A\" to toggle skybox", 5.0f, 85.0f, VulkanTextOverlay::alignLeft);
-		textOverlay->addText("\"Button X\" to toggle object", 5.0f, 100.0f, VulkanTextOverlay::alignLeft);
+		textOverlay->addText("Base material: " + materials[materialIndex].name + " (L1/R1)", 5.0f, 85.0f, VulkanTextOverlay::alignLeft);
+		textOverlay->addText("\"X\" to toggle object", 5.0f, 100.0f, VulkanTextOverlay::alignLeft);
 #else
-		textOverlay->addText("\"F2\" to toggle skybox", 5.0f, 85.0f, VulkanTextOverlay::alignLeft);
+		textOverlay->addText("Base material: " + materials[materialIndex].name + " (-/+)", 5.0f, 85.0f, VulkanTextOverlay::alignLeft);
 		textOverlay->addText("\"space\" to toggle object", 5.0f, 100.0f, VulkanTextOverlay::alignLeft);
 #endif
 	}
