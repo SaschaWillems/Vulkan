50e28c0bbaa76c6b62dfbd3b85b22a4a49f8a657
saschawillems <webmaster@saschawillems.de>
2017-06-15 11:25:06 +0200

    Moved shaders for triangle example into separate directory

--- data/shaders/spirv-triangle.bat
+++
@@ -1,3 +0,0 @@
-glslangvalidator -V triangle.vert -o triangle.vert.spv
-glslangvalidator -V triangle.frag -o triangle.frag.spv
-
--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -966,7 +966,7 @@ public:
 		// Set pipeline stage for this shader
 		shaderStages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
 		// Load binary SPIR-V shader
-		shaderStages[0].module = loadSPIRVShader(getAssetPath() + "shaders/triangle.vert.spv");
+		shaderStages[0].module = loadSPIRVShader(getAssetPath() + "shaders/triangle/triangle.vert.spv");
 		// Main entry point for the shader
 		shaderStages[0].pName = "main";
 		assert(shaderStages[0].module != VK_NULL_HANDLE);
@@ -976,7 +976,7 @@ public:
 		// Set pipeline stage for this shader
 		shaderStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
 		// Load binary SPIR-V shader
-		shaderStages[1].module = loadSPIRVShader(getAssetPath() + "shaders/triangle.frag.spv");
+		shaderStages[1].module = loadSPIRVShader(getAssetPath() + "shaders/triangle/triangle.frag.spv");
 		// Main entry point for the shader
 		shaderStages[1].pName = "main";
 		assert(shaderStages[1].module != VK_NULL_HANDLE);
