c77a1ba71546d94fcfa28db7bbc3375fc84eb7e1
saschawillems <webmaster@saschawillems.de>
2017-11-04 10:33:55 +0100

    Removed key dummy codes for android

--- base/keycodes.hpp
+++ base/keycodes.hpp
@@ -29,29 +29,16 @@
 #define KEY_N 0x4E
 #define KEY_O 0x4F
 #define KEY_T 0x54
-#elif defined(__ANDROID__)
-// Dummy key codes 
-#define KEY_ESCAPE 0x0
-#define KEY_F1 0x1
-#define KEY_F2 0x2
-#define KEY_F2 0x11
-#define KEY_F2 0x12
-#define KEY_F3 0x13
-#define KEY_F4 0x14
-#define KEY_W 0x3
-#define KEY_A 0x4
-#define KEY_S 0x5
-#define KEY_D 0x6
-#define KEY_P 0x7
-#define KEY_SPACE 0x8
-#define KEY_KPADD 0x9
-#define KEY_KPSUB 0xA
-#define KEY_B 0xB
-#define KEY_F 0xC
-#define KEY_L 0xD
-#define KEY_N 0xE
-#define KEY_O 0xF
-#define KEY_T 0x10
+
+#elif defined(VK_USE_PLATFORM_ANDROID_KHR)
+#define GAMEPAD_BUTTON_A 0x1000
+#define GAMEPAD_BUTTON_B 0x1001
+#define GAMEPAD_BUTTON_X 0x1002
+#define GAMEPAD_BUTTON_Y 0x1003
+#define GAMEPAD_BUTTON_L1 0x1004
+#define GAMEPAD_BUTTON_R1 0x1005
+#define GAMEPAD_BUTTON_START 0x1006
+#define TOUCH_DOUBLE_TAP 0x1100
 
 #elif defined(VK_USE_PLATFORM_IOS_MVK)
 // Use numeric keys instead of function keys.
@@ -130,13 +117,3 @@
 #define KEY_O 0x20
 #define KEY_T 0x1C
 #endif
-
-// todo: Android gamepad keycodes outside of define for now
-#define GAMEPAD_BUTTON_A		0x1000
-#define GAMEPAD_BUTTON_B		0x1001
-#define GAMEPAD_BUTTON_X		0x1002
-#define GAMEPAD_BUTTON_Y		0x1003
-#define GAMEPAD_BUTTON_L1		0x1004
-#define GAMEPAD_BUTTON_R1		0x1005
-#define GAMEPAD_BUTTON_START	0x1006
-#define TOUCH_DOUBLE_TAP		0x1100
