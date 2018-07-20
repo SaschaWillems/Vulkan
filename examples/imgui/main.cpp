4f8175506a0fb0abc6a902ac06ff6698619cee61
Giuseppe Barbieri <elect86@gmail.com>
2018-07-20 21:15:52 +0200

    Update main.cpp

--- examples/imgui/main.cpp
+++ examples/imgui/main.cpp
@@ -403,7 +403,6 @@ public:
 		}
 
 		// Index buffer
-		VkDeviceSize indexSize = imDrawData->TotalIdxCount * sizeof(ImDrawIdx);
 		if ((indexBuffer.buffer == VK_NULL_HANDLE) || (indexCount < imDrawData->TotalIdxCount)) {
 			indexBuffer.unmap();
 			indexBuffer.destroy();
