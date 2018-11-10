33870e46e331c63a56c4d47edf81a1a14152834b
saschawillems <webmaster@saschawillems.de>
2018-11-10 20:32:48 +0100

    Code cleanup
    Fixes #462

--- examples/renderheadless/renderheadless.cpp
+++ examples/renderheadless/renderheadless.cpp
@@ -789,10 +789,9 @@ public:
 			file << "P6\n" << width << "\n" << height << "\n" << 255 << "\n";
 
 			// If source is BGR (destination is always RGB) and we can't use blit (which does automatic conversion), we'll have to manually swizzle color components
-			bool colorSwizzle = false;
 			// Check if source is BGR and needs swizzle
 			std::vector<VkFormat> formatsBGR = { VK_FORMAT_B8G8R8A8_SRGB, VK_FORMAT_B8G8R8A8_UNORM, VK_FORMAT_B8G8R8A8_SNORM };
-			colorSwizzle = (std::find(formatsBGR.begin(), formatsBGR.end(), VK_FORMAT_R8G8B8A8_UNORM) != formatsBGR.end());
+			const bool colorSwizzle = (std::find(formatsBGR.begin(), formatsBGR.end(), VK_FORMAT_R8G8B8A8_UNORM) != formatsBGR.end());
 
 			// ppm binary pixel data
 			for (int32_t y = 0; y < height; y++) {
