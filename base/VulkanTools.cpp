13d8fc652621bdda40bba96ad8456f0d363709d3
Sascha Willems <webmaster@saschawillems.de>
2022-05-14 11:45:44 +0200

    Format selection list

--- base/VulkanTools.cpp
+++ base/VulkanTools.cpp
@@ -79,7 +79,7 @@ namespace vks
 			// Since all depth formats may be optional, we need to find a suitable depth format to use
 			// Start with the highest precision packed format
 			std::vector<VkFormat> depthFormats = {
-				//VK_FORMAT_D32_SFLOAT_S8_UINT,
+				VK_FORMAT_D32_SFLOAT_S8_UINT,
 				VK_FORMAT_D32_SFLOAT,
 				VK_FORMAT_D24_UNORM_S8_UINT,
 				VK_FORMAT_D16_UNORM_S8_UINT,
