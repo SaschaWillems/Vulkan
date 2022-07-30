05fa98f26bc8141ca537f6bac2666bec3d1c988d
Sascha Willems <webmaster@saschawillems.de>
2022-07-30 09:16:36 +0200

    Change light pos

--- examples/graphicspipelinelibrary/graphicspipelinelibrary.cpp
+++ examples/graphicspipelinelibrary/graphicspipelinelibrary.cpp
@@ -23,7 +23,7 @@ public:
 	struct UBOVS {
 		glm::mat4 projection;
 		glm::mat4 modelView;
-		glm::vec4 lightPos = glm::vec4(0.0f, 2.0f, 1.0f, 0.0f);
+		glm::vec4 lightPos = glm::vec4(0.0f, -2.0f, 1.0f, 0.0f);
 	} uboVS;
 	vks::Buffer uniformBuffer;
 
