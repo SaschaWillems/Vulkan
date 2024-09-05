5144cb8a99a74d985c629fb41a4af6fe58851ee2
Sascha Willems <webmaster@saschawillems.de>
2024-09-05 21:51:57 +0200

    Fix annotations for WinMain

--- base/Entrypoints.h
+++ base/Entrypoints.h
@@ -22,7 +22,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)				
 	}																								\
 	return (DefWindowProc(hWnd, uMsg, wParam, lParam));												\
 }																									\
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)									\
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR, _In_ int) \
 {																									\
 	for (int32_t i = 0; i < __argc; i++) { VulkanExample::args.push_back(__argv[i]); };  			\
 	vulkanExample = new VulkanExample();															\
