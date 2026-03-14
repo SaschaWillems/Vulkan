6b58e12e37b413bdd7c529b3cf11706151197d7e
SRSaunders <82544213+SRSaunders@users.noreply.github.com>
2026-03-13 21:20:59 -0400

    Add new SceneDelegate class files to iOS sub-project

---
+++ apple/ios/SceneDelegate.h
@@ -0,0 +1,16 @@
+/*
+ *  SceneDelegate.h
+ *
+ *  Copyright (c) 2016-2026 The Brenwill Workshop Ltd.
+ *  This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIViewController *viewController;
+
+@end
+
